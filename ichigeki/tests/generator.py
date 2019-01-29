ca942ac125bbd184445ac5ee26572d406d2d71eb
matsu7874 <mtsmtkmt@gmail.com>
2019-01-30 01:21:34 +0900

    #7 ichigekiの問題を追加

---
+++ ichigeki/tests/generator.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from collections import namedtuple
+import random
+NUM_TESTCASE = 40
+
+Case = namedtuple('Case', ('N', 'K', 'F', 'B'))
+
+N_MIN = 1
+N_MAX = 100000
+K_MIN = 0
+K_MAX = 100000
+F_MIN = 1
+F_MAX = 100000
+B_MIN = 1
+B_MAX = 100000
+"""
+1 <= N <= 100000
+0 <= K <= 100000
+
+1 <= F_i <= 100000
+1 <= B_i <= 100000
+"""
+testcases = set()
+
+testcases.add(Case(
+    4, 1,
+    (1, 3, 4, 5),
+    (4, 2, 1, 1)
+))
+size_testcase = len(testcases)
+
+while size_testcase < NUM_TESTCASE:
+    n = random.randint(N_MIN, N_MAX)
+    k = random.randint(K_MIN, K_MAX)
+    f = (random.randint(F_MIN, F_MAX) for _ in range(n))
+    b = (random.randint(B_MIN, B_MAX) for _ in range(n))
+    if Case(n, k, f, b) not in testcases:
+        testcases.add(Case(n, k, f, b))
+        size_testcase += 1
+
+for i, (n, k, f, b) in enumerate(list(testcases)):
+    with open('input_{}.in'.format(i), 'w') as fout:
+        f_str = ' '.join(map(str, f))
+        b_str = ' '.join(map(str, b))
+        fout.write('{} {}\n{}\n{}\n'.format(str(n), str(k), f_str, b_str))
---
+++ ichigeki/tests/validator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+n, k = map(int, input().split())
+f = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+assert 1 <= n <= 100000
+assert 1 <= k <= 100000
+assert all(1 <= fi <= 100000 for fi in f)
+assert all(1 <= bi <= 100000 for bi in b)
