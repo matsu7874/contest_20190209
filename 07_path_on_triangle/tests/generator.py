3f70c148923fa759da30de787f2101a2a59ba473
matsu7874 <mtsmtkmt@gmail.com>
2019-02-03 04:07:14 +0900

    07三角形のテストケースを作成

--- 07_path_on_triangle/tests/generator.py
+++ 07_path_on_triangle/tests/generator.py
@@ -1,1 +1,72 @@
 #!/usr/bin/env python3
+
+from collections import namedtuple
+import random
+
+NUM_TESTCASE = 40
+
+MAX_N = 10**3
+MIN_N = 1
+MAX_C = 10
+MIN_C = 1
+
+Case = namedtuple('Case', ('N', 'G'))
+
+
+def generate_graph(n):
+    graph = [(0,)]
+    for i in range(1,n):
+        row = []
+        for j in range(i+1):
+            row.append(random.randint(MIN_C, MAX_C))
+        if i == n-1:
+            row[0] = 0
+            row[n-1] = 0
+        graph.append(tuple(row))
+    return Case(
+        n,
+        tuple(graph)
+    )
+
+
+def main():
+    testcases = set()
+    testcases.add(
+        Case(
+            3,
+            (
+                (0,),
+                (4, 1),
+                (0, 2, 0)
+            )
+        )
+    )
+    testcases.add(
+        Case(
+            5,
+            (
+                (0,),
+                (9, 1),
+                (9, 1, 9),
+                (1, 5, 1, 1),
+                (0, 1, 1, 9, 0),
+            )
+        )
+    )
+    size_testcase = len(testcases)
+    while size_testcase < NUM_TESTCASE:
+        n = random.randint(MIN_N, MAX_N)
+        testcase = generate_graph(n)
+        if testcase not in testcases:
+            testcases.add(testcase)
+            size_testcase += 1
+
+    for i, (n, g) in enumerate(list(testcases)):
+        with open('input_{}.in'.format(i), 'w') as fout:
+            fout.write('{}\n'.format(n))
+            for row in g:
+                fout.write('{}\n'.format(' '.join(map(str, row))))
+
+
+if __name__ == "__main__":
+    main()
--- 07_path_on_triangle/tests/validator.py
+++ 07_path_on_triangle/tests/validator.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+MAX_N = 10**3
+MIN_N = 1
+MAX_C = 10
+MIN_C = 1
+
+n = int(input())
+assert MIN_N <= n <= MAX_N
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+assert len(graph) == n
+assert graph[0][0] == 0
+assert graph[n-1][0] == 0
+assert graph[n-1][n-1] == 0
+
+for i in range(n):
+    assert len(graph[i]) == i+1
+
+for i in range(1, n-1):
+    for c in graph[i]:
+        assert MIN_C <= c <= MAX_C
+for c in graph[n-1][1:-1]:
+    assert MIN_C <= c <= MAX_C
