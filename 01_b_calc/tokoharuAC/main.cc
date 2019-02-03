305c953c018246669440e2f3a48b3044c6387850
Shogo Kishimoto <tokoharu.sakura@gmail.com>
2019-02-04 01:38:54 +0900

    add tokoharu's 01 solution

---
+++ 01_b_calc/tokoharuAC/main.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+
+LL decode(string s) {
+    LL ret = 0;
+    for(int i=0; i<s.size(); i++) {
+        ret *= 2;
+        if(s[i] == 'B') ret++;
+    }
+    return ret;
+}
+
+string encode(LL n) {
+    vector<int> data;
+    while(n != 0) {
+        data.push_back(n%2);
+        n /= 2;
+    }
+    if(data.empty()) data = {0};
+    reverse(data.begin(), data.end());
+    string ret = "";
+    for(int i=0; i<data.size(); i++) {
+        if(data[i] == 1) ret += "B";
+        else ret += "b";
+    }
+    return ret;
+} 
+
+
+int main() {
+    string s;
+    string t;
+    cin >> s >> t;
+    cout << encode(decode(s) + decode(t)) << endl;   
+    
+    return 0;
+}
